07999f1a0fcdc9e640027d8ad4481cd84000b40f
IGSRIRAM <gnaneswarasriramindana@gmail.com>
2024-09-19 12:37:48 +0530

    new

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -12,34 +12,33 @@ function Dashboard() {
     switch (menuType) {
       case 'cameradrones':
         setSideNavItems([
-          { label: 'Aerial Photography', style: { fontWeight: 'bold',  color: '#212529', fontSize: '15px' }},
-          { label: 'DJI Mavic', link: 'djimavic' , style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' }},
-          { label: ' DJI Air', link: 'djiair'  , style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' }},
-          { label: ' DJI Mini', link: 'djimini' , style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' }},
-          { label: 'Immersive Flight Experience' , style: { fontWeight: 'bold',  color: '#212529', fontSize: '15px' }},
+          { label: 'Aerial Photography', style: { fontWeight: 'bold', color: '#212529', fontSize: '15px' } },
+          { label: 'DJI Mavic', link: 'djimavic', style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' } },
+          { label: ' DJI Air', link: 'djiair', style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' } },
+          { label: ' DJI Mini', link: 'djimini', style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' } },
+          { label: 'Immersive Flight Experience', style: { fontWeight: 'bold', color: '#212529', fontSize: '15px' } },
           { label: ' DJI Avata', link: 'djiavata', style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' } },
-          { label: ' DJI FPV', link: 'djifpv'  , style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' }},
-          { label: 'Aerial Cinematic Tools' , style: { fontWeight: 'bold',  color: '#212529', fontSize: '15px' }},
-          { label: 'DJI Inspire', link: 'djiinspire'  , style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px'}},
+          { label: ' DJI FPV', link: 'djifpv', style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' } },
+          { label: 'Aerial Cinematic Tools', style: { fontWeight: 'bold', color: '#212529', fontSize: '15px' } },
+          { label: 'DJI Inspire', link: 'djiinspire', style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' } },
         ]);
         break;
       case 'handheld':
         setSideNavItems([
-          { label: 'Record Your Life', style: { fontWeight: 'bold',  color: '#212529', fontSize: '15px' }},
-          { label: 'Osmo Pocket', link: 'osmopocket' , style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' }},
-          { label: 'Osmo Action', link: 'osmoaction' , style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' }},
-          { label: 'Osmo Mobile', link: 'osmomobile' , style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' }},
-          { label: 'DJI Mic', link: 'djimic' , style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' }},
-
-          { label: 'Professional Shooting', style: { fontWeight: 'bold',  color: '#212529', fontSize: '15px' }},
-          { label: 'Ronin Stabilizers', link: 'roninstabilizers' , style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' }},
-          { label: 'Ronin Cinema Cameras', link: 'ronincinemacameras' , style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' }},
+          { label: 'Record Your Life', style: { fontWeight: 'bold', color: '#212529', fontSize: '15px' } },
+          { label: 'Osmo Pocket', link: 'osmopocket', style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' } },
+          { label: 'Osmo Action', link: 'osmoaction', style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' } },
+          { label: 'Osmo Mobile', link: 'osmomobile', style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' } },
+          { label: 'DJI Mic', link: 'djimic', style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' } },
+          { label: 'Professional Shooting', style: { fontWeight: 'bold', color: '#212529', fontSize: '15px' } },
+          { label: 'Ronin Stabilizers', link: 'roninstabilizers', style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' } },
+          { label: 'Ronin Cinema Cameras', link: 'ronincinemacameras', style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' } },
         ]);
         break;
       case 'power':
         setSideNavItems([
-          { label: ' Portable Power Station', style: { fontWeight: 'bold',  color: '#212529', fontSize: '15px' }},
-          { label: 'DJI Power', link: 'djipower' , style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' }},
+          { label: 'Portable Power Station', style: { fontWeight: 'bold', color: '#212529', fontSize: '15px' } },
+          { label: 'DJI Power', link: 'djipower', style: { fontWeight: 'bold', color: '#5e6a75', fontSize: '18px' } },
         ]);
         break;
       default:
@@ -74,18 +73,17 @@ function Dashboard() {
         <Container>
           <Row className="w-100 align-items-center">
             <Col lg={1} className="d-flex align-items-center">
-              <Navbar.Brand href="#">DJI</Navbar.Brand>
-
+              <Navbar.Brand href="#" onClick={() => setSideNavItems([])}>
+                DJI
+              </Navbar.Brand>
             </Col>
             <Col lg={9} className="d-flex justify-content-around">
-              <Nav className="d-flex justify-content-center" style={{ gap: '30px',marginLeft: '50px' }}>
-             
+              <Nav className="d-flex justify-content-center" style={{ gap: '30px', marginLeft: '50px' }}>
                 <Nav.Link as={Link} to="/dashboard/cameradrones" onClick={() => handleTopMenuClick('cameradrones')}>Camera Drones</Nav.Link>
                 <Nav.Link as={Link} to="/dashboard/handheld" onClick={() => handleTopMenuClick('handheld')}>Handheld</Nav.Link>
                 <Nav.Link as={Link} to="/dashboard/power" onClick={() => handleTopMenuClick('power')}>Power</Nav.Link>
                 <Nav.Link as={Link} to="#services">Services</Nav.Link>
                 <Nav.Link as={Link} to="#accessories">Accessories</Nav.Link>
-                {/* Add more links as needed */}
               </Nav>
             </Col>
             <Col lg={2} className="d-flex align-items-center justify-content-end">
@@ -99,30 +97,30 @@ function Dashboard() {
         </Container>
       </Navbar>
 
-      <Container >
+      <Container>
         <Row>
           {/* Side Navigation */}
-          <Col  className="side-nav" >
-            <nav>
-              <ul>
-                {sideNavItems.map((item, index) => (
-                  <li key={index}>
-                    <Link to={item.link || '#'} style={item.style}>
-                      {item.label}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </Col>
-
+          {sideNavItems.length > 0 && (
+            <Col className="side-nav">
+              <nav>
+                <ul>
+                  {sideNavItems.map((item, index) => (
+                    <li key={index}>
+                      <Link to={item.link || '#'} style={item.style}>
+                        {item.label}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </Col>
+          )}
 
           {/* Dynamic Content */}
           <Col className="content">
             <Outlet />
           </Col>
         </Row>
-
       </Container>
     </div>
   );
